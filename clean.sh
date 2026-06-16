bbf59c169876cde4c19b0f3203a14af0877a68fe
Raphael <gld141@mail.usask.ca>
2026-06-16 15:20:58 -0600

    update scripts

--- clean.sh
+++ clean.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# This script removes all generated files from the output/ directory.
+# This script removes all generated files from the output/ directory
+# and unstages them from Git.
 
-echo "--- Cleaning output directory ---"
+echo "--- Cleaning output directory and Git index ---"
 
-# The -f flag prevents an error if the directory is empty
-rm -f output/*
+# Use git rm to remove files from both the working directory and the staging area.
+# The --ignore-unmatch flag prevents an error if there are no files to remove.
+# The -f flag forces removal of modified files.
+# The -q flag suppresses output for each file removed.
+git rm -fq --ignore-unmatch output/*
 
 echo "--- Cleanup complete ---"
